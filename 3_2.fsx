9e40ba7d31aafae6715ed3aed97dfcd17f67e369
Ilya Kovalenko <243144@niuitmo.ru>
2022-12-25 19:42:05 +0400

    make  3_2.fsx uint64

--- 3_2.fsx
+++ 3_2.fsx
@@ -1,11 +1,11 @@
-let rec f32 n x a = 
+let rec f32 (n:uint64) (x:uint64) a = 
     if x = n then
         x::a
-    elif n % x = 0 then 
+    elif n % x = 0UL then 
         f32 (n/x) x (x::a)
     else
-        f32 n (x+1) a
-let factorise n = f32 n 2 []
-let factors = factorise 13195
+        f32 n (x+1UL) a
+let factorise (n:uint64) = f32 n 2UL []
+let factors:uint64 list = factorise  (uint64 "600851475143")
 
-printfn "%A" factors[0]
+printfn "%A" factors.[0]
